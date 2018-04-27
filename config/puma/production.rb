47fa24b4e34a59d57a47dd0624d7ab290e479293
Virola <virola.zhu@gmail.com>
2018-04-27 20:34:09 +0800

    update port, finally solved the mina deploy

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -14,8 +14,8 @@ state_path "#{application_path}/shared/tmp/sockets/puma.state"
 stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
 #绑定sock
 bind "unix://#{application_path}/shared/tmp/sockets/puma.sock"
-#绑定端口
-bind 'tcp://127.0.0.1:8002'
+#绑定端口,nginx 是这个端口
+bind 'tcp://127.0.0.1:3000'
 worker_timeout 60
 daemonize true
 
