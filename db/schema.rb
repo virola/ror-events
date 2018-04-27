8370042d4a94077fef98a160d6d1f7685e42a323
Virola <virola.zhu@gmail.com>
2018-04-27 17:10:42 +0800

    annoying secrets..

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,7 @@ ActiveRecord::Schema.define(version: 2018_04_24_132018) do
     t.date "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    # t.string "email", default: "", null: false
-    # t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
